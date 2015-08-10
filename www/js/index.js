77f929281487dab2c3d7a11d4dd819eefd608c10
lskannel <gsalguero@gmail.com>
2015-08-10 16:35:27 -0600

    file test

--- www/js/index.js
+++ www/js/index.js
@@ -16,8 +16,105 @@ $(document).ready(function(e)
 	 
 	 if (!window.sessionStorage.UserLogin)
 	 	window.location = "#inicio";
+		
+	window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;	
+		
+	
 	 
 });
+
+function loadArchivo()
+{
+	window.requestFileSystem(window.TEMPORARY, 5*1024*1024, onInitFs, errorHandler);
+	//window.requestFileSystem(window.TEMPORARY, 0, onFileSystemSuccess, fail);
+}
+
+function toArray(list) {
+  return Array.prototype.slice.call(list || [], 0);
+}
+
+function listResults(entries) {
+  // Document fragments can improve performance since they're only appended
+  // to the DOM once. Only one browser reflow occurs.
+  var fragment = document.createDocumentFragment();
+
+  entries.forEach(function(entry, i) {
+    var img = entry.isDirectory ? '<img src="folder-icon.gif">' :
+                                  '<img src="file-icon.gif">';
+    var li = document.createElement('li');
+    li.innerHTML = [img, '<span>', entry.name, '</span>'].join('');
+    fragment.appendChild(li);
+  });
+
+  document.querySelector('#filelist').appendChild(fragment);
+}
+
+function onInitFs(fs) {
+
+  var dirReader = fs.root.createReader();
+  var entries = [];
+
+  // Call the reader.readEntries() until no more results are returned.
+  var readEntries = function() {
+     dirReader.readEntries (function(results) {
+      if (!results.length) {
+        listResults(entries.sort());
+      } else {
+        entries = entries.concat(toArray(results));
+        readEntries();
+      }
+    }, errorHandler);
+  };
+
+  readEntries(); // Start reading dirs.
+
+}
+
+/*
+function onFileSystemSuccess(fileSystem) 
+{
+    console.log(fileSystem.root);
+	
+	fileSystem.root.getDirectory(
+	
+	/*fileSystem.root.getFile('com.kannel.sqladminMovil.*', {}, 
+	function (fileEnt)
+	{
+		fileEnt.file(function (file)
+		{
+			var reader = new FileReader();
+			
+			reader.onloadend = function (e)
+			{
+				console.log(this.result);	
+			};
+			
+			reader.readAsDataURL(file);			
+			
+		}, fail2);
+		
+	}, fail);
+	
+	
+}*/
+
+
+function errorHandler(evt) 
+{
+	alert(evt.message);
+    console.log(evt);
+}
+
+function fail(evt) 
+{
+    console.log(evt.message);
+}
+
+function fail2(evt) 
+{
+	alert(evt.message);
+    console.log(evt.message);
+}
 
 $(function ()
 {
